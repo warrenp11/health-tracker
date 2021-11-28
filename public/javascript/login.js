ab6dea7cf85710ba7f18528a85cf59580cb9e7a6
Hunter <88465412+Hunyeh@users.noreply.github.com>
2021-11-28 09:21:57 -0500

    login.js and login.handlebars updated

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -4,14 +4,22 @@ async function signupFormHandler(event) {
     const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
+    const age = document.querySelector('#age-signup').value.trim();
+    const gender = document.querySelector('#gender-signup').value.trim();
+    const height = document.querySelector('#height-signup').value.trim();
+    const weight = document.querySelector('#weight-signup').value.trim();
 
-    if (username && email && password) {
+    if (username && email && password && age && gender && height && weight) {
         const response = await fetch('/api/users', {
             method: 'post',
             body: JSON.stringify({
                 username,
                 email,
-                password
+                password,
+                age,
+                gender,
+                height,
+                weight
             }),
             headers: { 'Content-Type': 'application/json' }
         });
