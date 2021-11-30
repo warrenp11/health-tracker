1d0c942330c68eeabffcc072ac9b3a313ccfe2e3
Hunter <88465412+Hunyeh@users.noreply.github.com>
2021-11-30 17:40:56 -0500

    seperated login/sign up. edited some handlebars

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,38 +1,10 @@
-async function signupFormHandler(event) {
-    event.preventDefault();
-
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-    const age = document.querySelector('#age-signup').value.trim();
-    const gender = document.querySelector('#gender-signup').value.trim();
-    const height = document.querySelector('#height-signup').value.trim();
-    const weight = document.querySelector('#weight-signup').value.trim();
-
-    if (username && email && password && age && gender && height && weight) {
-        const response = await fetch('/api/users', {
-            method: 'post',
-            body: JSON.stringify({
-                username,
-                email,
-                password,
-                age,
-                gender,
-                height,
-                weight
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        });
-        if (response.ok) {
-            console.log('success');
-        } else {
-            alert(response.statusText);
-        }
-    }
-};
-
 async function loginFormHandler(event) {
     event.preventDefault();
+
+    console.log(event)
+    const formData = new FormData(event.target);
+    const formProps = Object.fromEntries(formData);
+    console.log(formProps)
 
     const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
@@ -54,5 +26,5 @@ async function loginFormHandler(event) {
     }
 };
 
-document.querySelector('#signup').addEventListener('submit', signupFormHandler);
-document.querySelector('#login').addEventListener('submit', loginFormHandler);
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
