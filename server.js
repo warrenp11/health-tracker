957724e257c2a85b68aee4f97a51dfeed2143e71
Hunter <88465412+Hunyeh@users.noreply.github.com>
2021-11-29 19:25:52 -0500

    added session

--- server.js
+++ server.js
@@ -2,14 +2,28 @@ const path = require("path");
 const express = require("express");
 const exphbs = require('express-handlebars');
 const routes = require("./controllers");
+const session = require('express-session');
 const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 // Set up Handlebars.js as engine of choice
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ });
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
