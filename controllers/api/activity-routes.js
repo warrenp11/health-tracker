a97c5a2a140b2bf21dd11f8fd494d00877f2e8bb
Jagy <jagy.das12@gmail.com>
2021-11-29 19:37:35 -0500

    updated routes

--- controllers/api/activity-routes.js
+++ controllers/api/activity-routes.js
@@ -17,5 +17,94 @@ router.get("/", (req, res) => {
         });
 });
 
+// find activity by id
+
+router.get('/:id', (req, res) => {
+    // find one category by its `id` value
+    // be sure to include its associated Products
+    Activity.findOne({
+            where: {
+                id: req.params.id
+            },
+            include: {
+                model: User,
+                attributes: ['id']
+            }
+        })
+        .then(dbActivityData => {
+            if (!dbActivityData) {
+                res.status(404).json({ message: 'No activties found' });
+                return;
+            }
+            res.json(dbActivityData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err)
+        });
+});
+
+// POST /api/users
+router.post("/", (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+    Activity.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+        })
+        .then((dbUserData) => res.json(dbUserData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+
+// PUT /api/activities/1
+router.put("/:id", (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+
+    // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+    Activity.update(req.body, {
+            individualHooks: true,
+            where: {
+                id: req.params.id,
+            },
+        })
+        .then((dbUserData) => {
+            if (!dbUserData[0]) {
+                res.status(404).json({ message: "No user found with this id" });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// DELETE /api/activities/1
+router.delete("/:id", (req, res) => {
+    Activity.destroy({
+            where: {
+                id: req.params.id,
+            },
+        })
+        .then((dbUserData) => {
+            if (!dbUserData) {
+                res.status(404).json({ message: "No user found with this id" });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+
+
 
 module.exports = router;
--- controllers/api/exercise-routes.js
+++ controllers/api/exercise-routes.js
@@ -17,5 +17,28 @@ router.get("/", (req, res) => {
         });
 });
 
+// GET /api/exercises/1
+router.get("/:id", (req, res) => {
+    Exercise.findOne({
+            attributes: ['id', 'exerciseName', 'videoLink', 'category'],
+            where: {
+                id: req.params.id,
+            },
+        })
+        .then((dbExerciseData) => {
+            if (!dbExerciseData) {
+                res.status(404).json({ message: "No Exercise tied this id" });
+                return;
+            }
+            res.json(dbExerciseData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+
+module.exports = router;
 
 module.exports = router;
