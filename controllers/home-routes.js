25c4246859491f4329b1181a44f6ec48b2e987e9
Peter <warrenp11@gmail.com>
2021-12-01 18:13:58 -0500

    user dashboard populating user info

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,28 @@
+const sequelize = require("../config/connection");
+const { User, Exercise, Activity } = require("../models");
+
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-  res.render("dashboard");
+  User.findAll({
+    attributes: [
+      "id",
+      "username",
+      "email",
+      "height",
+      "weight",
+      "age",
+      "gender",
+    ],
+  })
+    .then((dbUserData) => {
+      // console.log(dbUserData[0]);
+      res.render("dashboard", dbUserData[1].get({ plain: true }));
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get("/login", (req, res) => {
