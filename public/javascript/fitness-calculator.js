711a469347a2fd350a8cedfad74ec7a488a0729f
Peter <warrenp11@gmail.com>
2021-12-07 09:37:33 -0500

    recoded bmi submit event handler, still not functioning

--- public/javascript/fitness-calculator.js
+++ public/javascript/fitness-calculator.js
@@ -1,16 +1,20 @@
 const fitnessCalculatorFunctions = require("fitness-calculator");
 
-const bmiBtnEl = document.querySelector("#bmi-btn");
-
-function bmiCalculator(event) {
+async function bmiFormHandler(event) {
     event.preventDefault();
     // console.log(event);
+    
+    const height = document.querySelector("#height").value.trim();
+    const weight = document.querySelector("#weight").value.trim();
+    // console.log(height);
+    // console.log(weight);
 
-    const height= document.querySelector("#height").value.trim();
-    const weight= document.querySelector("#weight").value.trim();
+    if (height && weight) {
+        const myBmi = fitnessCalculatorFunctions(height, weight);
+        console.log(myBmi);
+    }
 
-    const myBmi = fitnessCalculatorFunctions(height, weight);
-    console.log(myBmi);
 }
 
-bmiBtnEl.addEventListener("click", bmiCalculator);
+
+document.querySelector("#bmi-form").addEventListener("submit", bmiFormHandler);
